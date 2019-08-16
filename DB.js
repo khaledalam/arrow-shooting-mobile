7987bc2260a291f82dc9e484542b0a4f268eb827
Khaled Alam <khaledalam.net@gmail.com>
2019-08-16 07:29:08 +0200

    upload DB

---
+++ DB.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+
+
+var config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+firebase.initializeApp(config);
